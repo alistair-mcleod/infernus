d5de49b512c7f9cdef294a9a6a9ccc13091d1943
Alistair <Eedles@github.com>
2023-11-21 16:50:41 +1100

    test pull request
    asdasdadasdasdasdsadsadsadsad

--- cleanup_job.sh
+++ cleanup_job.sh
@@ -2,7 +2,7 @@
 ##SBATCH --job-name=cleanup_doubletest2
 #SBATCH --output=triton_logs/%x.log
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=20G
 #SBATCH --time=80:00:00
 
 python infernus/megacleanup.py --savedir=$1 --injfile=$2

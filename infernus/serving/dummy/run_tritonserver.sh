2e812588c6017157cf998d3a8ff0dc386d211834
Alistair <Eedles@github.com>
2024-08-20 18:41:59 +1000

    updating arguments

--- infernus/serving/dummy/run_tritonserver.sh
+++ infernus/serving/dummy/run_tritonserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#argument 1 is port 
+#argument 1 is the HTTP port. The other ports are allocated sequentially.
 port=$1
 
 http_port=$port
@@ -10,22 +10,11 @@ metrics_port=$((port+2))
 echo GRPC port is $grpc_port
 
 savedir=${2} 
-#/model_repositories/repo_1
 echo $savedir
 
-# TensorFlow Model
-# apptainer run --nv -B /fred/oz016/damon/infernus/infernus/serving/dummy/model_repository:/models /fred/oz016/damon/containers/tritonserver.sif tritonserver --model-repository=/models --backend-config=tensorflow,version=2 --exit-on-error=false
+serverdir=${3}
 
-# ONNX Model
-#apptainer run --nv -B /fred/oz016/damon/infernus/infernus/serving/dummy/model_repository:/models /fred/oz016/damon/containers/tritonserver.sif tritonserver --model-repository=/models --backend-config=onnxruntime,default-max-batch-size=512 --exit-on-error=false
-#apptainer run --nv -B /fred/oz016/alistair/infernus/infernus/serving/dummy/model_repository:/models /fred/oz016/damon/containers/tritonserver.sif tritonserver --model-repository=/models --backend-config=onnxruntime,default-max-batch-size=512 --exit-on-error=false
-apptainer run --nv -B ${savedir}:/models /fred/oz016/damon/triton_server/containers_22.11/tritonserver.sif tritonserver --model-repository=/models --backend-config=onnxruntime,default-max-batch-size=512 --exit-on-error=true --http-port $http_port --grpc-port $grpc_port --metrics-port $metrics_port
+# Start the triton server
+#/fred/oz016/damon/triton_server/containers_22.11/tritonserver.sif
 
-#/fred/oz016/alistair/infernus/infernus/serving/dummy/model_repository
-
-#to use A100s use /fred/oz016/damon/containers/tritonserver.sif
-#to use P100s switch to 22.11 container /fred/oz016/damon/triton_server/containers_22.11/tritonserver.sif
-
-# Custom Ports
-# apptainer run --nv -B /fred/oz016/damon/infernus/infernus/serving/dummy/model_repository:/models /fred/oz016/damon/containers/tritonserver.sif tritonserver --model-repository=/models --backend-config=onnxruntime,default-max-batch-size=512 --exit-on-error=false --http-port 8005 --grpc-port 8006 --metrics-port 8007
-
+apptainer run --nv -B ${savedir}:/models ${serverdir} tritonserver --model-repository=/models --backend-config=onnxruntime,default-max-batch-size=512 --exit-on-error=true --http-port $http_port --grpc-port $grpc_port --metrics-port $metrics_port
--- share/cleanup_job.sh
+++ share/cleanup_job.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 #SBATCH --output=triton_logs/%x.log
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=60G
-#SBATCH --time=60:00:00
+#SBATCH --time=100:00:00
 
 infernus_dir="/fred/oz016/alistair/infernus"
 
-python $infernus_dir/infernus/megacleanup.py --savedir=$1 --injfile=$2
+python $infernus_dir/infernus/megacleanup.py --savedir=$1 --jsonfile=$2

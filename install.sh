fa46077f8966cc602c9564d81bda4344d47f44e4
Alistair <Eedles@github.com>
2025-11-20 14:13:05 +1100

    updating installation + triton requirements

--- install.sh
+++ install.sh
@@ -19,34 +19,69 @@ else
 fi
 
 #add user input to confirm that they want to install into the current environment
-# read -p "Do you want to install Infernus into the current environment? (y/n): " confirm
-# if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
-# 	echo "Installation halted. Please ensure you have activated a Python virtual environment for installing Infernus."
-# 	exit 1
-# fi
-
-# pip install .
-
-#check if GWSamplegen is already installed
-#isInstalled=$(pip show GWSamplegen | grep -c "Name: GWSamplegen")
-isInstalled=0
-if [[ $isInstalled -eq 0 ]]; then
-	echo "GWSamplegen is not installed. It will be installed into the parent directory of Infernus."
-	read -p "Do you want to install GWSamplegen into $(dirname $INFERNUS_DIR)/GWSamplegen? (y/n): " confirm
-	if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
-		echo "Skipping GWSamplegen installation. Note that Infernus will not work without GWSamplegen."
-		exit 0
-	fi
-	#install GWSamplegen from GitHub
-	echo "Installing GWSamplegen from GitHub..."
-	#put it in the parent directory of the current working directory
-	git clone git@github.com:alistair-mcleod/GWSamplegen.git ../GWSamplegen
-	cd ../GWSamplegen
-	#pip install .
-	cd "${INFERNUS_DIR}"
+read -p "Do you want to install Infernus into the current environment? (y/n): " confirm
+if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+	echo "Not installing into a virtual environment. Please ensure you have a virtual environment with Infernus installed."
+	#echo "Installation halted. Please ensure you have activated a Python virtual environment for installing Infernus."
+	#exit 1
+else
+	pip install .
 fi
 
-#also add the virtual environment as INFERNUS_ENV
+
+#check if GWSamplegen is already installed
+#note there's two things to check: if the venv already has GWSamplegen, and if the parent directory has GWSamplegen
+isInVenv=$(pip show GWSamplegen | grep -cw "Name: GWSamplegen")
+isInParentDir=$(ls $(dirname $INFERNUS_DIR) | grep -cw "GWSamplegen")
+# isInVenv=0
+# isInParentDir=0
+echo "Checking for GWSamplegen installation..."
+
+if [[ $isInParentDir -eq 0 ]]; then
+	read -p "Do you want to clone GWSamplegen into $(dirname $INFERNUS_DIR)/GWSamplegen? (y/n): " confirm
+	if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+		echo "Skipping GWSamplegen installation in parent directory."
+		if [[ $isInVenv -eq 0 ]]; then
+			echo "GWSamplegen is not installed in the current virtual environment. Note Infernus will not work without GWSamplegen."
+		fi
+		#exit 0
+	else
+		echo "Installing GWSamplegen from GitHub..."
+		#put it in the parent directory of the current working directory
+		git clone git@github.com:alistair-mcleod/GWSamplegen.git ../GWSamplegen
+		#add the GWSamplegen directory as GWSAMPLEGEN_DIR in ~/.bashrc
+		if ! grep -q "export GWSAMPLEGEN_DIR=" ~/.bashrc; then
+			echo "Appending GWSAMPLEGEN_DIR to ~/.bashrc"
+			echo "export GWSAMPLEGEN_DIR=\"$(dirname $INFERNUS_DIR)/GWSamplegen\"" >> ~/.bashrc
+		else
+			echo "GWSAMPLEGEN_DIR already set in ~/.bashrc. Overwriting..."
+			#overwrite the existing line to ensure it points to the correct directory
+			sed -i "s|^export GWSAMPLEGEN_DIR=.*|export GWSAMPLEGEN_DIR=\"$(dirname $INFERNUS_DIR)/GWSamplegen\"|" ~/.bashrc
+			echo "Updated GWSAMPLEGEN_DIR in ~/.bashrc"
+		fi
+	fi
+
+	if [[ $isInVenv -eq 0 ]]; then
+	read -p "Do you want to install GWSamplegen into the current virtual environment? (y/n): " confirm
+		if [[ ! "$confirm" =~ ^[Yy]$ ]]; then
+			echo "Skipping GWSamplegen installation in "
+		else
+			echo "Installing..."
+			cd ../GWSamplegen
+			bash install.sh
+			pip install .
+			cd "${INFERNUS_DIR}"
+		fi
+	fi
+fi
+
+if [[ $isInVenv -eq 1 && $isInParentDir -eq 1 ]]; then
+	echo "GWSamplegen is already installed in both the current virtual environment and the parent directory. Skipping installation."
+fi
+
+
+#also add the virtual environment as INFERNUS_ENV. 
+#TODO: This is deprecated and should be removed
 if ! grep -q "export INFERNUS_ENV=" ~/.bashrc; then
 	echo "Appending INFERNUS_ENV to ~/.bashrc"
 	echo "export INFERNUS_ENV=\"${VIRTUAL_ENV}/bin/activate\"" >> ~/.bashrc
